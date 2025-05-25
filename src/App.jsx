e28080a432b8ece142ddf00c3f847cd033b4d511
Sara <sarailic431@gmail.com>
2025-05-25 19:34:06 +0200

    .....

--- src/App.jsx
+++ src/App.jsx
@@ -9,21 +9,11 @@ function App() {
 
   const [selectDifficulty, setSelectDifficulty] = useState("easy");
 
-  // useEffect(() => {
-  //   const fetchPost = async () => {
-  //     try {
-  //       const response = await fetch(
-  //         `https://thecocktaildb.com/api/json/v1/1/filter.php?i=Gin`
-  //       );
-  //       const posts = await response.json();
-  //       setCoctails(posts.drinks);
-  //     } catch (error) {
-  //       console.log(error);
-  //     }
-  //   };
+  const [displayQuestions, setDisplayQuestions] = useState(false);
 
-  //   fetchPost();
-  // }, []);
+  const [question, setQuestion] = useState({});
+
+  const [number, setNumber] = useState(0);
 
   function handleSelectCategory(e) {
     setSelectCategory(e.target.value);
@@ -37,7 +27,8 @@ function App() {
     e.preventDefault();
     console.log(inputNumber, selectCategory, selectDifficulty);
 
-    let categoryNumber = 21;
+    let categoryNumber = "";
+    if (selectCategory === "sports") categoryNumber = 21;
     if (selectCategory === "geography") categoryNumber = 22;
     if (selectCategory === "mythology") categoryNumber = 20;
     if (selectCategory === "art") categoryNumber = 25;
@@ -50,6 +41,16 @@ function App() {
         const posts = await response.json();
         console.log(posts);
         console.log(posts.results);
+        console.log(posts.results[number].question);
+        console.log(posts.results[number].correct_answer);
+        console.log(posts.results[number].incorrect_answers);
+
+        setDisplayQuestions(true);
+        setQuestion({
+          question: posts.results[number].question,
+          correct_answer: posts.results[number].correct_answer,
+          incorrect_answers: posts.results[number].incorrect_answers,
+        });
       } catch (error) {
         console.log(error);
       }
@@ -60,54 +61,69 @@ function App() {
 
   return (
     <>
-      <div className="form-container">
-        <h1>Quiz setup</h1>
+      {!displayQuestions && (
+        <div className="form-container">
+          <h1>Quiz setup</h1>
 
-        <form className="setup-form" onSubmit={handleSubmit}>
-          <label>Number of questions</label>
-          <input
-            type="number"
-            value={inputNumber}
-            onChange={(e) => setInputNumber(e.target.value)}
-          />
-          <br />
-          <label>Category</label>
-          <select className="select-category" onChange={handleSelectCategory}>
-            <option value="sports">Sports</option>
-            <option value="geography">Geography</option>
-            <option value="mythology">Mythology</option>
-            <option value="art">Art</option>
-          </select>
-          <br />
-          <label>Difficulty</label>
-          {/* prettier-ignore */}
-          <select className="select-difficulty" onChange={handleSelectDifficulty}>
+          <form className="setup-form" onSubmit={handleSubmit}>
+            <label>Number of questions</label>
+            <input
+              type="number"
+              value={inputNumber}
+              onChange={(e) => setInputNumber(e.target.value)}
+            />
+            <br />
+            <label>Category</label>
+            <select className="select-category" onChange={handleSelectCategory}>
+              <option value="sports">Sports</option>
+              <option value="geography">Geography</option>
+              <option value="mythology">Mythology</option>
+              <option value="art">Art</option>
+            </select>
+            <br />
+            <label>Difficulty</label>
+            {/* prettier-ignore */}
+            <select className="select-difficulty" onChange={handleSelectDifficulty}>
             <option value="easy">Easy</option>
             <option value="medium">Medium</option>
             <option value="hard">Hard</option>
           </select>
-          <br />
-          <button>Start playing!</button>
-        </form>
-      </div>
-
-      <main className="questions-main hidden">
-        <div className="score-tracker">Correct answers: 0 / 0</div>
-        <h2 className="question">
-          Who won the 2015 Formula 1 World Championship?
-        </h2>
-
-        <div className="answers-container">
-          <p className="single-question">Nico Rosberg</p>
-          <p className="single-question">Sebastian Vettel</p>
-          <p className="single-question">Jenson Button</p>
-          <p className="single-question">Lewis Hamilton</p>
+            <br />
+            <button>Start playing!</button>
+          </form>
         </div>
+      )}
 
-        <div className="next-question-div">
-          <button className="next-question-btn">Next question</button>
-        </div>
-      </main>
+      {displayQuestions && (
+        <main className="questions-main">
+          <div className="score-tracker">Correct answers: 0 / 0</div>
+          <h2 className="question">{question.question}</h2>
+
+          <div className="answers-container">
+            <p className="single-question" onClick={() => nextQuestion()}>
+              {question.correct_answer}
+            </p>
+            {/* <p className="single-question">Sebastian Vettel</p>
+            <p className="single-question">Jenson Button</p>
+            <p className="single-question">Lewis Hamilton</p> */}
+
+            {question.incorrect_answers.map((answer, index) => (
+              <p
+                key={index}
+                className="single-question"
+                onClick={() => nextQuestion()}
+              >
+                {" "}
+                {answer}{" "}
+              </p>
+            ))}
+          </div>
+
+          <div className="next-question-div">
+            <button className="next-question-btn">Next question</button>
+          </div>
+        </main>
+      )}
     </>
   );
 }
